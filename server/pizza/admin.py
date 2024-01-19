b11ffb3ade28af42f9a22b3efed205281ab835de
John <johnnkonge2020@gmail.com>
2024-01-19 07:56:08 +0300

    made admin list

--- server/pizza/admin.py
+++ server/pizza/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza,Restaurant,RestPizza
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('name','price','size')
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('restaurant_name','address','city')
+
+class RestPizzaAdmin(admin.ModelAdmin):
+    list_display = ('pizza','rest_info','price')
+
+admin.site.register(Pizza,PizzaAdmin)
+admin.site.register(Restaurant,RestaurantAdmin)
+admin.site.register(RestPizza,RestPizzaAdmin)
