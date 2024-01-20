82eb1abbe209e773f4496b08171de895e9d1483e
John <johnnkonge2020@gmail.com>
2024-01-20 06:43:01 +0300

    yaay!

--- client/pizza/src/App.jsx
+++ client/pizza/src/App.jsx
@@ -1,18 +1,18 @@
-
 import { useEffect } from 'react';
 import './App.css';
 
 function App() {
  useEffect(()=>{
-  fetch("http://127.0.0.1:8000/api/restaurant/")
-    .then(function(response){
-    response.json().then(function(data) {
+  fetch("http://127.0.0.1:8000/api/pizza/")
+    .then(function(response){response.json()
+    .then(function(data) {
         console.log(data);
     });
-}).catch(function(error) {
+  })
+  .catch(function(error) {
     console.log('Fetch Error:', error);
-});
- },[])
+  });
+  },[])
   return (
     <div className="App">
       <h1>Welcome to Pizza HUB</h1>
