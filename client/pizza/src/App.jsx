2ab4cca49bf598fc2bd6cdb78f6ab94006533f4b
John <johnnkonge2020@gmail.com>
2024-01-20 06:33:05 +0300

    frontend communicating with backend

--- client/pizza/src/App.jsx
+++ client/pizza/src/App.jsx
@@ -3,13 +3,16 @@ import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  useEffect(()=>{
-    fetch('http://127.0.0.1:8000/api/restaurant/')
-      .then((response) => (response.json))
-      .then((pizzaList) => {
-        console.log(pizzaList)
-      })
-  },[])
+ useEffect(()=>{
+  fetch("http://127.0.0.1:8000/api/restaurant/")
+    .then(function(response){
+    response.json().then(function(data) {
+        console.log(data);
+    });
+}).catch(function(error) {
+    console.log('Fetch Error:', error);
+});
+ },[])
   return (
     <div className="App">
       <h1>Welcome to Pizza HUB</h1>
