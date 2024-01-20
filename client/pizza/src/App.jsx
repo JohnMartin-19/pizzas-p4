c5863f539bb2f9dffbb064bf1236a33723f1e858
John <johnnkonge2020@gmail.com>
2024-01-20 06:25:41 +0300

    updated CORS

--- client/pizza/src/App.jsx
+++ client/pizza/src/App.jsx
@@ -1,7 +1,15 @@
 
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    fetch('http://127.0.0.1:8000/api/restaurant/')
+      .then((response) => (response.json))
+      .then((pizzaList) => {
+        console.log(pizzaList)
+      })
+  },[])
   return (
     <div className="App">
       <h1>Welcome to Pizza HUB</h1>
