a4ded2e927b07afbdfc7fc12342f1b35ccdf36ec
John <johnnkonge2020@gmail.com>
2024-01-20 13:46:42 +0300

    landing page,done!

--- client/pizza/src/Home.jsx
+++ client/pizza/src/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import './Home.css';
+import lp from './img/pizzalp.jpg';
 function Home(){
     return(
-        <div>Home Page</div>
+    
+    <div  className="hero">
+      
+        <div className="mask">
+        <img className="intro-img" src={lp} alt="landing page" />
+        </div>
+        <div className="content">
+            <p>Hello There</p>
+            <h2 style={{color:'white'}}>WELCOME TO PIZZAHUB</h2>
+            <Link to='/Pizza' className="btn">Pizza</Link>
+            <Link className="btn btn-light" to='/Restaurants'>Restaurants</Link>
+        </div>
+        
+    </div>
+        
     )
 }
 export default Home
