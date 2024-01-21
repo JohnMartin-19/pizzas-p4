2302dd33ff8a681138f1b15f528522d4f8ef83fb
John <johnnkonge2020@gmail.com>
2024-01-21 14:55:56 +0300

    updating frontend

--- client/pizza/src/Restaurants.jsx
+++ client/pizza/src/Restaurants.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect} from "react";
-
+import {Link} from 'react-router-dom'
 function Restaurants(){
     useEffect(()=>{
         fetch("http://127.0.0.1:8000/api/restaurant/")
@@ -12,8 +12,20 @@ function Restaurants(){
             console.log('Fetch Error:', error);
           });
         },[])
-    return(
-        <div>Restaurants</div>
+  return(
+      <div  className="hero">
+      
+        <div className="mask">
+        
+        </div>
+        <div className="content">
+            <p>Hello There</p>
+            <h2 style={{color:'white'}}>VIEW OUR RESTAURANTS</h2>
+            <Link to='/Pizza' className="btn">Pizza</Link>
+            <Link className="btn btn-light" to='/Restaurants'>Restaurants</Link>
+        </div>
+      
+      </div>
     )
 }
 
