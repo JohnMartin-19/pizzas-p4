82d855dad271bd35161ddc19d9cd6725f7749390
John <johnnkonge2020@gmail.com>
2024-01-21 14:43:05 +0300

    updated the css

--- client/pizza/src/Pizza.jsx
+++ client/pizza/src/Pizza.jsx
@@ -1,28 +1,37 @@
-import React,{useEffect} from "react";
-import {Link} from 'react-router-dom'
+import React,{useEffect, useState} from "react";
+import lp from './img/pizzalp.jpg';
+import './Pizza.css'
 function Pizza(){
+    const [pizza,setPizza] = useState([])
     useEffect(()=>{
         fetch("http://127.0.0.1:8000/api/pizza/")
           .then(function(response){response.json()
           .then(function(data) {
               console.log(data);
+              setPizza(data)
           });
         })
           .catch(function(error) {
             console.log('Fetch Error:', error);
           });
-        },[])
+        },[setPizza])
     return(
-        <div  className="hero">
-      
-        <div className="mask">
-        <img className="intro-img"  alt="landing page" />
-        </div>
+    <div  className="hero">
         <div className="content">
-            <p>Hello There</p>
-            <h2 style={{color:'white'}}>WELCOME TO PIZZAHUB</h2>
-            <Link to='/Pizza' className="btn">Pizza</Link>
-            <Link className="btn btn-light" to='/Restaurants'>Restaurants</Link>
+            <h2 style={{color:'white'}} >VIEW OUR AVAILABLE PIZZAS</h2>
+            {pizza.map((pizza) => (
+            <div className="card" key={pizza.id}>
+            <img src={lp} alt="my avatar"  />
+            <br />
+            <small id="name">Name: {pizza.name}</small>
+            <br />
+            <small id="topping">Topping: {pizza.toppings}</small>
+            <br />
+            <small id="size">Size: {pizza.size}</small>
+            <br />
+            
+            </div>
+             ))}
         </div>
         
     </div>
