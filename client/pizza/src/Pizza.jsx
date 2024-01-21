2e165532d2c6157fa02501b7a645d636049111b7
John <johnnkonge2020@gmail.com>
2024-01-21 14:04:36 +0300

    updating components

--- client/pizza/src/Pizza.jsx
+++ client/pizza/src/Pizza.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect} from "react";
-
+import {Link} from 'react-router-dom'
 function Pizza(){
     useEffect(()=>{
         fetch("http://127.0.0.1:8000/api/pizza/")
@@ -13,7 +13,23 @@ function Pizza(){
           });
         },[])
     return(
-        <div>Welcome to Pizza Page</div>
+        <div  className="hero">
+      
+        <div className="mask">
+        <img className="intro-img"  alt="landing page" />
+        </div>
+        <div className="content">
+            <p>Hello There</p>
+            <h2 style={{color:'white'}}>WELCOME TO PIZZAHUB</h2>
+            <Link to='/Pizza' className="btn">Pizza</Link>
+            <Link className="btn btn-light" to='/Restaurants'>Restaurants</Link>
+        </div>
+        
+    </div>
+        
+
     )
 }
+
+
 export default Pizza
